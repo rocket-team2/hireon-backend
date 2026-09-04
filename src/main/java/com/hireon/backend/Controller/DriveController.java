9fd22da08dff95406c55724d373d8627794e6128
vishnu <vishnusankar0506@gmail.com>
2026-09-04 17:57:33 +0530

    Temp

--- src/main/java/com/hireon/backend/Controller/DriveController.java
+++ src/main/java/com/hireon/backend/Controller/DriveController.java
@@ -1,46 +1,15 @@
 package com.hireon.backend.Controller;
 import com.hireon.backend.Model.Drive;
 import com.hireon.backend.Service.DriveService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/drives")
+@RequestMapping("/Drives")
 public class DriveController {
-    private final DriveService driveService;
-    public DriveController(DriveService driveService){
-        this.driveService=driveService;
-    }
-    @PostMapping
-    public ResponseEntity<Drive> createDrive(@RequestBody Drive drive){
-        Drive createdDrive=driveService.createDrive(drive);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdDrive);
-    }
-    @GetMapping
-    public ResponseEntity<List<Drive>> getAllDrives(){
-        List<Drive> drives=driveService.getAllDrives();
-        return ResponseEntity.ok(drives);
-    }
-    @GetMapping("/{id}")
-    public ResponseEntity<Drive> getDriveById(
-        @PathVariable Integer id) {
-    Drive drive = driveService.getDriveById(id);
-    return ResponseEntity.ok(drive);
-    }
-    @PutMapping("/{id}")
-    public ResponseEntity<Drive> updateDrive(
-            @PathVariable Integer id,
-            @RequestBody Drive drive
-    ){
-        Drive updatedDrive=driveService.updateDrive(id,drive);
-        return ResponseEntity.ok(updatedDrive);
-    }
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteDrive(
-            @PathVariable Integer id) {
-        driveService.deleteDrive(id);
-        return ResponseEntity.noContent().build();
-    }
+    @Autowired
+    private DriveService driveService;
 }
--- src/main/java/com/hireon/backend/Service/DirectorService.java
+++ src/main/java/com/hireon/backend/Service/DirectorService.java
@@ -9,21 +9,4 @@ import org.springframework.stereotype.Service;
 public class DirectorService {
     @Autowired
     private DirectorRepo directorRepo;
-    public Director register(Director director) {
-
-        return directorRepo.save(director);
-    }
-
-    public Director getDirector(Long id) {
-        return directorRepo.getById(id);
-    }
-
-    public Director updateDirector(Long id, Director request) {
-        Director director=directorRepo.findById(id)
-                .orElseThrow(()-> new RuntimeException("Director Not Found"));
-        director.setName(request.getName());
-        director.setEmail(request.getEmail());
-
-        return directorRepo.save(director);
-    }
 }
