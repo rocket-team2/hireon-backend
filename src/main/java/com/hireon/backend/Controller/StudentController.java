28503c005a977b33a2994dbc0d49cb9d60d2e9dd
vishnu <vishnusankar0506@gmail.com>
2026-09-04 11:38:24 +0530

    Student completed
    # Conflicts:
    #	src/main/java/com/hireon/backend/Model/User.java

--- src/main/java/com/hireon/backend/Controller/StudentController.java
+++ src/main/java/com/hireon/backend/Controller/StudentController.java
@@ -1,4 +1,61 @@
 package com.hireon.backend.Controller;
 
+import com.hireon.backend.Model.Student;
+import com.hireon.backend.Service.StudentService;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/Student")
+@Slf4j
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/getnull")
+    public Student getNull(){
+        return new Student();
+    }
+
+    @PostMapping("/add")
+    public Student addStudent(@RequestBody Student student) {
+        log.info("inside controller");
+        return studentService.addStudent(student);
+    }
+
+    @GetMapping("/Id/{id}")
+    public Student getStudent(@PathVariable long id){
+        return studentService.getStudent(id);
+    }
+
+    @GetMapping()
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/Name/{name}")
+    public Optional<Student> getStudentByName(@PathVariable String name){
+        return studentService.getStudentByName(name);
+    }
+
+    @PutMapping("/Update")
+    public Student updateStudent(@RequestBody Student student){
+        return studentService.updateStudent(student);
+    }
+
+    @PutMapping("/Status")
+    public Student updateStatus(@RequestParam Long id,String status,long comp_id){
+        return studentService.updateStatus(id,status,comp_id);
+    }
+
+    @DeleteMapping("/Delete/{id}")
+    public String deleteStudent(@PathVariable long id){
+        return studentService.deleteStudent(id);
+    }
+
 }
--- src/main/java/com/hireon/backend/Repository/StudentRepo.java
+++ src/main/java/com/hireon/backend/Repository/StudentRepo.java
@@ -1,4 +1,12 @@
 package com.hireon.backend.Repository;
 
-public interface StudentRepo {
+import com.hireon.backend.Model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student,Long> {
+    Optional<Student> findByName(String name);
 }
