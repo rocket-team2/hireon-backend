4b1b634cdd8847e6012f819c1430e556c446c552
iyalarasuofficial <iyalarasuofficial@gmail.com>
2026-09-05 11:43:31 +0530

    director service

--- src/main/java/com/hireon/backend/Service/DirectorService.java
+++ src/main/java/com/hireon/backend/Service/DirectorService.java
@@ -9,4 +9,22 @@ import org.springframework.stereotype.Service;
 public class DirectorService {
     @Autowired
     private DirectorRepo directorRepo;
+    public Director register(Director director) {
+
+        return directorRepo.save(director);
+    }
+
+    public Director getDirector(Long id) {
+        return directorRepo.getById(id);
+    }
+
+    public Director updateDirector(Long id, Director request) {
+        Director director=directorRepo.findById(id)
+                .orElseThrow(()-> new RuntimeException("Director Not Found"));
+        director.setName(request.getName());
+        System.out.println(request.getName());
+        director.setEmail(request.getEmail());
+
+        return directorRepo.save(director);
+    }
 }
